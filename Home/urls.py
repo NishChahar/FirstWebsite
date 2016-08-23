82ad18adc551829e4b76db6ac699bdd9a8bf0339
Nishant Chahar <chaharnishant07@gmail.com>
2016-08-23 22:43:40 +0530

    Addition of Favorite method

--- Home/urls.py
+++ Home/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'Home'
+
 urlpatterns = [
     #/Home/
     url(r'^$', views.index, name='index'),
 
     #/Home/123(id)
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    #/Home/Favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album,Song
 
 
 def index(request):
@@ -9,8 +8,20 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    return render(request, 'Home/detail.html', {'album': album})
+   album = get_object_or_404(Album, id=album_id)
+   return render(request, 'Home/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+   album = get_object_or_404(Album, id=album_id)
+   try:
+       selected_song = album.song_set.get(id=request.POST['song'])
+   except (KeyError, Song.DoesNotExist):
+       return render(request, 'Home/detail.html', {
+           'album': album,
+           'error_message': "You did not select a valid song",
+       })
+   else:
+       selected_song.is_favorite = True
+       selected_song.save()
+       return render(request, 'Home/detail.html', {'album': album})
