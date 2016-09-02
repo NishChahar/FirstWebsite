70fb63a39a07fb10fa68025e5cfafbb92a1472fd
Nishant Chahar <chaharnishant07@gmail.com>
2016-09-02 17:46:04 +0530

    Changed the look of Home Page
    Changed how the Home Page used to look and also added the functionality
    to add and album by the user

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,8 @@
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album
+
 
 class IndexView(generic.ListView):
     template_name = 'Home/index.html'
@@ -11,3 +14,18 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Album
     template_name = 'Home/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('Home:index')
