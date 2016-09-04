1c735b20f29a3741a5a8079a657ec935b54ddc7f
Nishant Chahar <chaharnishant07@gmail.com>
2016-09-04 16:47:13 +0530

    Completed almost all frontend
    Added song creation views and favourite view although favorite views
    needs more work on it. Done with almost all of the frontend work just
    need a good color scheme.

--- FirstWebsite/urls.py
+++ FirstWebsite/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -6,3 +7,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^Home/', include('Home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
