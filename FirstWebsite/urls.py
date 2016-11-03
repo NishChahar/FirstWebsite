456563b99e1c8f056e397dc7c603829b85c2246e
Nishant Chahar <chaharnishant07@gmail.com>
2016-11-03 19:58:26 +0530

    Solved Sign In issue

--- FirstWebsite/urls.py
+++ FirstWebsite/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    ##url(r'^$', views.base,),
     url(r'^admin/', admin.site.urls),
     url(r'^Home/', include('Home.urls')),
 ]
